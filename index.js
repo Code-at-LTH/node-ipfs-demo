01d8dc1385538589f25eca5f5244668abc85cc64
Jacob Karlsson <jacob.karlsson95@gmail.com>
2018-02-21 16:13:49 +0100

    Finish demo

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("hello world")
+'use strict'
+// ipfsd-ctl is used to control a go-ipfs daemon from js
+// (js-ipfs isn't mature enough do to certain things like IPNS yet)
+// This requires that you have go-ipfs-dep installed so that you have
+// an executable to run.
+const factory = require('ipfsd-ctl').create()
+
+factory.spawn((err, ipfsd) => {
+    if (err) { throw err }
+    const ipfs = ipfsd.api
+
+    ipfs.id().then(id => {
+        console.log("id:", id)
+    }).catch(console.error)
+
+    ipfs.files.add([{
+        path: '/test-file.txt',
+        content: Buffer.from('This is my test file', 'utf-8')
+    }]).then(res => {
+        const fileHash = res[0].hash
+        console.log('Added file with hash:', fileHash)
+
+        return ipfs.files.cat(fileHash)
+    }).then(text => {
+        console.log('File contents:', text.toString())
+    }).catch(console.error)
+})
